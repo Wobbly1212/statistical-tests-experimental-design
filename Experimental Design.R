e1b3fddba52795f2991b0ff67745a4d370c571a9
HOSSEIN DARABI <163558782+Wobbly1212@users.noreply.github.com>
2025-04-06 16:03:28 +0200

    Update Experimental Design.R

--- Experimental Design.R
+++ Experimental Design.R
@@ -480,6 +480,68 @@ shapiro.test(mice2$after-mice2$before)
 
 res <- t.test(mice2$after,mice2$before, paired = TRUE)
 res
+###########################################################################
+######################### Non-parametric tests ############################
+###########################################################################
+
+##############################################
+### One-sample Wilcoxon signed rank test #####
+##############################################
+
+set.seed(127)
+
+my_data <- data.frame(
+  name = paste0(rep("M_", 30), 1:30),
+  weight = round(rnorm(10, 20, 12), 3)
+)
+
+my_data
+
+hist(my_data$weight)
+shapiro.test(my_data$weight)
+
+boxplot(my_data$weight, ylab = "Weight (g)")
+
+# We want to know, if the average weight of the mice differs from 25g 
+
+res <- wilcox.test(my_data$weight, mu = 25)
+res 
+
+# ties = pairs of data that have the same ordinal values
+# the average weight of the mice is significantly different from 25g 
+
+#########################################################
+### Two-samples Mann-Whitney-Wilcoxon rank-sum test #####
+#########################################################
+
+# independent groups
+
+women_weight <- c(38.9, 61.2, 73.3, 21.8, 63.4, 64.6, 48.4, 48.8, 48.5,48)
+men_weight <- c(67.8, 60, 63.4, 76, 89.4, 73.3, 67.3, 61.3, 62.4, 60) 
+
+hist(women_weight)
+hist(men_weight)
+
+shapiro.test(women_weight)
+shapiro.test(men_weight)
+
+my_data <- data.frame( 
+  group = rep(c("Woman", "Man"), each = 10),
+  weight = c(women_weight,  men_weight)
+)
+
+my_data
+
+# does the median of women's weight differ from the median of men's weight?
+
+library("ggpubr")
+ggboxplot(my_data, x = "group", y = "weight", 
+          color = "group", palette = c("red", "pink"),
+          ylab = "Weight", xlab = "Groups")
+
+res <- wilcox.test(women_weight, men_weight)
+res
+
 
 
 
